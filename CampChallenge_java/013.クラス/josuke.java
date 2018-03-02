72c4dce01f52208766f075b39d1e60a2321014fd
I120X\赤城優 <yu.osomatusan@gmail.com>
2018-03-02 17:41:13 +0900

    クラスの継承　提出します

---
+++ CampChallenge_java/013.クラス/josuke.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 赤城優
+ */
+ class josuke extends jojo{
+           
+          public static void main(String [] args){
+              
+            
+              
+              josuke diamond=new josuke();
+              
+              diamond.jinbutu("ギアッチョ","ホワイトアルバム");
+        
+        diamond.hyouji();
+              
+        diamond.dio();
+               
+        diamond.hyouji();
+       
+          }     
+          
+         public void dio(){
+             this.name=null;
+             this.standname=null;
+         }
+} 
+
+
+
+  
+
