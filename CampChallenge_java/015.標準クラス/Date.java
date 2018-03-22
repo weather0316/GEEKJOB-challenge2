3704ad3b31f575d1d7c0fe12eda6692b54457fcb
I120X\赤城優 <yu.osomatusan@gmail.com>
2018-03-22 13:22:24 +0900

    import文日付編課題1提出します

---
+++ CampChallenge_java/015.標準クラス/Date.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 赤城優
+ */
+import java.util.Calendar;
+import java.util.Date;
+
+class Sample {
+    public static void main(String[] args) {
+      Date d=new Date(1451574000000L);
+       
+       System.out.print(d);
+    }
+}
