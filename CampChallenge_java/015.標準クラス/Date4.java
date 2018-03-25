4f375e396336caf6030d8039934e36d25e04a23d
I120X\赤城優 <yu.osomatusan@gmail.com>
2018-03-25 22:03:14 +0900

    日付編課題4提出します

---
+++ CampChallenge_java/015.標準クラス/Date4.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 赤城優
+ */
+
+
+public class Date4 {
+    public static void main(String []args){
+ 
+        
+        Long s=(1420092000000L+315576000000L+2678400000L+82800000L+35400000L+59000)-1420092000000L;
+   System.out.print(s);
+}
+}
