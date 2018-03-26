926b6488640c6fa90fcbf39fb3f7df2d31dc3b6a
I120X\赤城優 <yu.osomatusan@gmail.com>
2018-03-26 13:07:26 +0900

    文字列課題3 提出します

---
+++ CampChallenge_java/015.標準クラス/string3.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 赤城優
+ */
+public class string3 {
+    public static void main(String[]args){
+        String bun="きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+        
+       String A=bun.replace("U","う");
+       System.out.print(A.replace("I","い"));
+ }
+}
