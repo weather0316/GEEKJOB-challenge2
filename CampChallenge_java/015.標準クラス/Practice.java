c2b93caf718396e59ebce75d54e052fce90099d8
I120X\赤城優 <yu.osomatusan@gmail.com>
2018-03-26 11:17:24 +0900

    文字列編課題1　提出します

---
+++ CampChallenge_java/015.標準クラス/Practice.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 赤城優
+ */
+public class Practice {
+    public static void main(String[] args){
+      String name = "赤城優";
+   
+      System.out.println(name.length());
+      System.out.println(name.getBytes().length);
+    }
+}
