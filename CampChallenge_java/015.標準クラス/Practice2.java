240b1e343d652a5f0c26cfd015e88736c853868a
I120X\赤城優 <yu.osomatusan@gmail.com>
2018-03-26 11:45:15 +0900

    文字列課題2 提出します

---
+++ CampChallenge_java/015.標準クラス/Practice2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 赤城優
+ */
+public class Practice2 {
+    public static void main(String[] args){
+      String adress= "melone.jojo@gmail.com";
+   
+      String A=adress.substring(12);
+      System.out.println(A.length());
+    }
+}
