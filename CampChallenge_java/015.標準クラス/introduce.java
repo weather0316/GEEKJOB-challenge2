b5e0e65d5522a10b37bdf107f2416df6c674427c
I120X\赤城優 <yu.osomatusan@gmail.com>
2018-03-26 15:53:03 +0900

    訂正

--- CampChallenge_java/015.標準クラス/introduce.java
+++ CampChallenge_java/015.標準クラス/introduce.java
@@ -8,6 +8,20 @@
  *
  * @author 赤城優
  */
+import java.io.*;
+
 public class introduce {
-    
+    public static void main(String[]args){
+        try{
+            File f=new File("introduce.text");
+            
+            FileWriter fw=new FileWriter(f);
+            fw.write("私の名前は赤城優です。20歳です");
+            
+            fw.close();
+            
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
